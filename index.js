0cb50e835dcde74501719a9dea3f81d739823ffc
stefan <stefan.ylimaki@gmail.com>
2022-09-21 15:32:50 +0300

    3.4 done

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-01-10T17:30:31.098Z",
-      important: true
+    { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
     },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-01-10T18:39:34.091Z",
-      important: false
+    { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
     },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-01-10T19:20:14.298Z",
-      important: true
+    { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+    },
+    { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
     }
 ]
   
@@ -46,6 +48,12 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
 })
 
 const PORT = 3001
