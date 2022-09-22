b976edf9cd35fc538caf2318d150231e55088e97
stefan <stefan.ylimaki@gmail.com>
2022-09-22 12:12:49 +0300

    3.11 done

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ morgan.token('body', req => {
     return JSON.stringify(req.body)
   })
 
+const cors = require('cors')
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -34,7 +37,6 @@ let persons = [
 ]
   
 app.get('/', function (req, res) {
-
     res.send('hello, world!')
   })
 
@@ -110,6 +112,7 @@ const unknownEndpoint = (request, response) => {
   
   app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
